c8440779e10eed2cace725918fed884ed607d5e2
FrancieliF <francielle-fernanda@hotmail.com>
2014-06-15 20:16:06 -0300

    Classe CategoriaPublicacao
    Criação do constrututor e dos métodos get e Set

--- app/models/CategoriaPublicacao.java
+++ app/models/CategoriaPublicacao.java
@@ -1,5 +1,25 @@
 package models;
 
 public class CategoriaPublicacao {
-
+	Long IDCategoriaPublicacao;
+	String CategoriaPublicacao;
+	public CategoriaPublicacao(Long iDCategoriaPublicacao,
+			String categoriaPublicacao) {
+		IDCategoriaPublicacao = iDCategoriaPublicacao;
+		CategoriaPublicacao = categoriaPublicacao;
+	}
+	public Long getIDCategoriaPublicacao() {
+		return IDCategoriaPublicacao;
+	}
+	public void setIDCategoriaPublicacao(Long iDCategoriaPublicacao) {
+		IDCategoriaPublicacao = iDCategoriaPublicacao;
+	}
+	public String getCategoriaPublicacao() {
+		return CategoriaPublicacao;
+	}
+	public void setCategoriaPublicacao(String categoriaPublicacao) {
+		CategoriaPublicacao = categoriaPublicacao;
+	}
+	
+	
 }
