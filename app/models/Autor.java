35aff1b5aedfa5e6d76f91661cdba8903dcf9418
FrancieliF <francielle-fernanda@hotmail.com>
2014-06-15 20:58:27 -0300

    Classe Autor
    Criação do construtor e métodos get e set para a classe

--- app/models/Autor.java
+++ app/models/Autor.java
@@ -1,5 +1,42 @@
 package models;
 
 public class Autor {
+	Long IDAutor;
+	String autor;
+	/**
+	 * @param iDAutor
+	 * @param autor
+	 */
+	public Autor(Long iDAutor, String autor) {
+		super();
+		IDAutor = iDAutor;
+		this.autor = autor;
+	}
+	/**
+	 * @return the iDAutor
+	 */
+	public Long getIDAutor() {
+		return IDAutor;
+	}
+	/**
+	 * @param iDAutor the iDAutor to set
+	 */
+	public void setIDAutor(Long iDAutor) {
+		IDAutor = iDAutor;
+	}
+	/**
+	 * @return the autor
+	 */
+	public String getAutor() {
+		return autor;
+	}
+	/**
+	 * @param autor the autor to set
+	 */
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	
 	
 }
